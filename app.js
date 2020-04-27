7effe2e47db2623cdd4b254e9ad4018d5746ee40
kahoul <ilyes.kahoul@epitech.eu>
2020-04-27 14:47:26 +0200

    core: don't connect to mongodb

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-require('./models');
+//require('./models');
 
 var app = express();
 
