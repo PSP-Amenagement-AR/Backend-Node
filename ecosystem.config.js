fdf234bc8c4d9139015b144c95eb8aa90c26dec3
Mavrick <Mavrick>
2020-10-26 14:32:34 +0100

    remove swagger from docker-compose.yml

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,15 +2,12 @@ module.exports = {
     apps : [{
         script: 'app.js',
         watch: '.'
-    }, {
-        script: './service-worker/',
-        watch: ['./service-worker']
     }],
 
     deploy : {
         production : {
-            user : 'SSH_USERNAME',
-            host : 'SSH_HOSTMACHINE',
+            user : 'ar-amenagement-vm',
+            host : '34.77.130.134',
             ref  : 'origin/master',
             repo : 'git@github.com:PSP-Amenagement-AR/Backend-Node.git',
             path : 'DESTINATION_PATH',
